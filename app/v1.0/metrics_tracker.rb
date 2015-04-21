3d92446a999e6b76fa35c8f0e0f1e641c7619e88
Matt Congel <conge101@mail.chapman.edu>
2015-04-21 10:44:51 -0700

    test api response

--- app/v1.0/metrics_tracker.rb
+++ app/v1.0/metrics_tracker.rb
@@ -15,21 +15,21 @@ get '/v1.0/stats' do
 
 	batch_req = [
 		{
-			"method" => "GET",
-			"name" => "get-url-stats",
-			"relative_url" => "v2.3/?id=#{url}",
-			"omit_response_on_success" => false
+			method: "GET",
+			name: "get-url-stats",
+			relative_url: "v2.3/?id=#{url}",
+			omit_response_on_success: false
 		},
 		{
-			"method" => "GET",
-			"name" => "likes",
-			"relative_url" => "v2.3/{result=get-url-stats:$.og_object.id}?fields=likes.summary(true).limit(0)"
+			method: "GET",
+			name: "likes",
+			relative_url: "v2.3/{result=get-url-stats:$.og_object.id}?fields=likes.summary(true).limit(0)"
 		}
 	]
 
 	puts "before route."
 
-	route = "https://graph.facebook.com/v2.3?access_token=#{CONFIG['fb_app_id']}|#{CONFIG['fb_app_secret']}&batch=#{batch_req}"
+	route = "https://graph.facebook.com/?access_token=#{CONFIG['fb_access_token']}&batch=#{URI.encode(batch_req.to_json)}"
 	response = HTTParty.post(route)
 
 	puts response.inspect
