9d92a65019a886a1ffdd929a5c920e04b13d7c0d
Matt Congel <conge101@mail.chapman.edu>
2015-04-21 11:03:46 -0700

    test api response

--- app/v1.0/metrics_tracker.rb
+++ app/v1.0/metrics_tracker.rb
@@ -13,26 +13,27 @@ get '/v1.0/stats' do
 	url = params[:url]
 	puts url
 
-	# uri = URI("https://graph.facebook.com/v2.3")
-	# req = Net::HTTP::Post.new(uri.path)
-	# attach = {'batch' => [{"method" => "GET","name" => "get-url-stats","relative_url" => "v2.3/?id=#{url}","omit_response_on_success" => false},{"method" => "GET","name" => "likes","relative_url" => "v2.3/{result=get-url-stats:$.og_object.id}?fields=likes.summary(true).limit(0)"}].to_json}
-	# req.set_form_data(attach.merge('access_token' => CONFIG['fb_access_token']))
+	uri = URI("https://graph.facebook.com/v2.3")
+	req = Net::HTTP::Post.new(uri.path)
+	attach = {'batch' => [{"method" => "GET","name" => "get-url-stats","relative_url" => "v2.3/?id=#{url}","omit_response_on_success" => false},{"method" => "GET","name" => "likes","relative_url" => "v2.3/{result=get-url-stats:$.og_object.id}?fields=likes.summary(true).limit(0)"}].to_json}
+	req.set_form_data(attach.merge('access_token' => CONFIG['fb_access_token']))
 
-	# res = Net::HTTP.new(uri.host, uri.port)
-	# res.verify_mode = OpenSSL::SSL::VERIFY_NONE
-	# res.use_ssl = true
+	res = Net::HTTP.new(uri.host, uri.port)
+	res.verify_mode = OpenSSL::SSL::VERIFY_NONE
+	res.use_ssl = true
 
-	# response = nil
-	# res.start do |http|
-	# 	response = http.request(req)
-	# 	puts response.inspect
-	# end
-
-	batch_req = [{"method" => "GET","name" => "get-url-stats","relative_url" => "v2.3/?id=#{url}","omit_response_on_success" => false},{"method" => "GET","name" => "likes","relative_url" => "v2.3/{result=get-url-stats:$.og_object.id}?fields=likes.summary(true).limit(0)"}].to_json
-
-	route = "https://graph.facebook.com/?access_token=#{CONFIG['fb_access_token']}&batch=#{URI.encode(batch_req)}"
-	response = HTTParty.post(route)
+	response = nil
+	res.start do |http|
+		response = http.request(req)
+	end
 
 	puts response.inspect
+
+	# batch_req = [{"method" => "GET","name" => "get-url-stats","relative_url" => "v2.3/?id=#{url}","omit_response_on_success" => false},{"method" => "GET","name" => "likes","relative_url" => "v2.3/{result=get-url-stats:$.og_object.id}?fields=likes.summary(true).limit(0)"}].to_json
+
+	# route = "https://graph.facebook.com/?access_token=#{CONFIG['fb_access_token']}&batch=#{URI.encode(batch_req)}"
+	# response = HTTParty.post(route)
+
+	# puts response.inspect
 
 end
