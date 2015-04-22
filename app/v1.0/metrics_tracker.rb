c22f03274b5c290398b8fee2cfc8d4535513f637
Matt Congel <conge101@mail.chapman.edu>
2015-04-22 13:05:51 -0700

    test api response

--- app/v1.0/metrics_tracker.rb
+++ app/v1.0/metrics_tracker.rb
@@ -13,19 +13,23 @@ get '/v1.0/stats' do
 	url = params[:url]
 	puts url
 
-	uri = URI("https://graph.facebook.com/v2.3")
-	req = Net::HTTP::Post.new(uri.path)
+	# uri = URI("https://graph.facebook.com/v2.3")
+	# req = Net::HTTP::Post.new(uri.path)
+	# attach = {'batch' => [{"method" => "GET","name" => "get-url-stats","relative_url" => "v2.3/?id=#{url}","omit_response_on_success" => false},{"method" => "GET","name" => "likes","relative_url" => "v2.3/{result=get-url-stats:$.og_object.id}?fields=likes.summary(true).limit(0)"}].to_json}
+	# req.set_form_data(attach.merge('access_token' => CONFIG['fb_access_token']))
+
 	attach = {'batch' => [{"method" => "GET","name" => "get-url-stats","relative_url" => "v2.3/?id=#{url}","omit_response_on_success" => false},{"method" => "GET","name" => "likes","relative_url" => "v2.3/{result=get-url-stats:$.og_object.id}?fields=likes.summary(true).limit(0)"}].to_json}
-	req.set_form_data(attach.merge('access_token' => CONFIG['fb_access_token']))
+	uri = URI("https://graph.facebook.com/v2.3")
+	uri.query = URI.encode_www_form(attach.merge('access_token' = CONFIG['fb_access_token']))
+	req = Net::HTTP::Get.new uri.request_uri
 
 	res = Net::HTTP.new(uri.host, uri.port)
 	res.verify_mode = OpenSSL::SSL::VERIFY_NONE
 	res.use_ssl = true
 
 	response = nil
-	response = res.start do |http|
-		post_data = URI.encode_www_form(req)
-		http.request(post_data)
+	res.start do |http|
+		response = http.request(req)
 	end
 
 	return JSON.parse(response.read_body)
