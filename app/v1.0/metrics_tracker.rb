184069437c8604169d053d8feec141af842ea1a3
Matt Congel <conge101@mail.chapman.edu>
2015-04-23 09:25:52 -0700

    test api response

--- app/v1.0/metrics_tracker.rb
+++ app/v1.0/metrics_tracker.rb
@@ -11,18 +11,18 @@ get '/v1.0/stats' do
 	url = params[:url]
 	puts url
 
-	uri = URI('https://graph.facebook.com/v2.3/')
+	uri = URI.parse('https://graph.facebook.com/v2.3/')
 	uri.query = Rack::Utils.build_query(
 		id: url,
-		fields: 'share, og_object{engagement}',
+		fields: 'share, og_object{id,url,engagement}',
 		access_token: CONFIG['fb_access_token']
 	)
 
-	req = Net::HTTP::Get.new(uri.query)
+	req = Net::HTTP::Get.new(uri.to_s)
 
 	#req.set_form_data('fields' => 'og_object[engagement]','access_token' => CONFIG['fb_access_token'])
 
-	puts req
+	puts req.inspect
 
 	res = Net::HTTP.new(uri.host, uri.port)
 	res.verify_mode = OpenSSL::SSL::VERIFY_NONE
