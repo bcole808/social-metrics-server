1d33cbb9ba63c8fb953d07b0d3d376518269c066
Matt Congel <conge101@mail.chapman.edu>
2015-04-23 08:33:49 -0700

    test api response

--- app/v1.0/metrics_tracker.rb
+++ app/v1.0/metrics_tracker.rb
@@ -8,8 +8,6 @@ CONFIG = YAML.load_file('./config/setup.yml')
 
 get '/v1.0/stats' do
 
-	puts "in do."
-
 	url = params[:url]
 	puts url
 
@@ -20,16 +18,9 @@ get '/v1.0/stats' do
 		access_token: CONFIG['fb_access_token']
 	)
 
-	req = Net::HTTP::Post.new(uri.query)
+	req = Net::HTTP::Get.new(uri.path)
 
-	#attach = {'batch' => [{"method" => "GET","name" => "get-url-stats","relative_url" => "v2.3/?id=#{url}","omit_response_on_success" => false},{"method" => "GET","name" => "get-url-stats","relative_url" => "v2.3/{result=get-url-stats:$.og_object.id}?fields=share,og_object.engagement"}].to_json}
-	#attach = {'batch' => [{"method" => "GET","name" => "get-url-stats","relative_url" => "v2.0/?id=#{url}","omit_response_on_success" => false},{"method" => "GET","name" => "likes","relative_url" => "v2.0/{result=get-url-stats:$.og_object.id}?fields=likes.summary(true).limit(0)"}].to_json}
 	#req.set_form_data('fields' => 'og_object[engagement]','access_token' => CONFIG['fb_access_token'])
-
-	# attach = {'batch' => [{"method" => "GET","name" => "get-url-stats","relative_url" => "v2.3/?id=#{url}","omit_response_on_success" => false},{"method" => "GET","name" => "likes","relative_url" => "v2.3/{result=get-url-stats:$.og_object.id}?fields=likes.summary(true).limit(0)"}].to_json}
-	# uri = URI("https://graph.facebook.com/v2.3")
-	# uri.query = URI.encode_www_form(attach.merge('access_token' => CONFIG['fb_access_token']))
-	# req = Net::HTTP::Get.new uri.request_uri
 
 	puts req
 
@@ -45,13 +36,5 @@ get '/v1.0/stats' do
 	output = ""
 	output << "#{response.body} <br />"
 	return output
-	
-
-	# batch_req = [{"method" => "GET","name" => "get-url-stats","relative_url" => "v2.3/?id=#{url}","omit_response_on_success" => false},{"method" => "GET","name" => "likes","relative_url" => "v2.3/{result=get-url-stats:$.og_object.id}?fields=likes.summary(true).limit(0)"}].to_json
-
-	# route = "https://graph.facebook.com/?access_token=#{CONFIG['fb_access_token']}&batch=#{URI.encode(batch_req)}"
-	# response = HTTParty.post(route)
-
-	# puts response.inspect
 
 end
